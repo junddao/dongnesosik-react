ee1e267ee85a3f4addf9077d8eefa8d60a8c6182
junddao <y2hunter@gmail.com>
2023-08-31 15:20:31 +0900

    Upt. react version

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
-import "./App.css";
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 import { Home } from "./components/Home";
 // import { About } from "./components/About";
-import { Services } from "./components/Services";
-import { Contact } from "./components/Contact";
-import { EmptyPage } from "./components/EmptyPage";
-import { NavigationBar } from "./components/NavigationBar";
-import { Footer } from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+import { NavigationBar } from "./components/NavigationBar";
+import { Services } from "./components/Services";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <NavigationBar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/services">
-            <Services />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route>
-            <EmptyPage></EmptyPage>
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
         <Footer />
       </div>
     </BrowserRouter>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { ReactComponent as BannerImage } from "../assets/img/banner_image_1.svg";
 import { Button, Card } from "react-bootstrap";
+import { ReactComponent as BannerImage } from "../assets/img/banner_image_1.svg";
 
 <svg
   xmlns="http://www.w3.org/2000/svg"
